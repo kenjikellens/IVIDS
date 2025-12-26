a8618761661990c41bd0fcfb95b84f607b45d37f
Kenji Kellens <kenji.kellens@student.hogent.be>
2025-12-26 22:02:35 +0100

    fix: correct app version to v0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kenjigames.ivids"
         minSdk = 21
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "v0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
