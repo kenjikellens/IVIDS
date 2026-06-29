48012563306eb4fe7531955c788142d41504419e
Kenji Kellens <kenji.kellens@student.hogent.be>
2026-06-29 10:04:36 +0200

    Release v0.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kenjigames.ivids"
         minSdk = 21
         targetSdk = 36
-        versionCode = 25
-        versionName = "v0.5.1"
+        versionCode = 27
+        versionName = "v0.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
