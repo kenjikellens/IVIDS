a2fe66db8b6015dd2564905d8a6aaca6ccb74637
Kenji Kellens <kenji.kellens@student.hogent.be>
2026-07-25 15:39:29 +0200

    feat: configure Android build settings and update version to v0.5.5 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.kenjigames.ivids"
         minSdk = 21
         targetSdk = 35
-        versionCode = 57
+        versionCode = 58
         versionName = "v0.5.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
