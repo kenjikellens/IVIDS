08af55cf3020f4cf942aa1269edc382f2bfbda8a
Kenji Kellens <kenji.kellens@student.hogent.be>
2026-09-16 18:17:06 +0200

    feat: add global and mobile stylesheets alongside spatial navigation script

--- app/src/main/assets/main/gui/js/spatial-nav.js
+++ app/src/main/assets/main/gui/js/spatial-nav.js
@@ -100,7 +100,7 @@ export const SpatialNav = {
      */
     findScrollParent(el, modal) {
         if (modal) {
-            const knownScrollArea = el.closest('.modal-scroll-area, .scrollable-modal-body, .select-options-list, .select-options-grid, .playlist-selection-list');
+            const knownScrollArea = el.closest('.language-grid, .language-options-list, .modal-scroll-area, .scrollable-modal-body, .modal-body, .select-options-list, .select-options-grid, .playlist-selection-list');
             if (knownScrollArea) return knownScrollArea;
 
             let parent = el.parentElement;
