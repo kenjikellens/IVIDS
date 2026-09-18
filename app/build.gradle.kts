32f5315ee83bf578bf9e2573a350ea9756727666
Kenji Kellens <kenji.kellens@student.hogent.be>
2026-09-18 10:11:58 +0200

    feat: add global mobile stylesheet and update build configurations and binaries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.kenjigames.ivids"
         minSdk = 21
         targetSdk = 35
-        versionCode = 90
+        versionCode = 91
         versionName = "v0.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
